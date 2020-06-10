88aaf99933710fdfeeea35f36ae03f27ce1121a7
mangojuicedeli <mgkang0317@gmail.com>
2020-06-10 10:34:17 +0900

    initial commit

---
+++ src/main/java/com/reddit/SpringbootWebservicePrac1Application.java
@@ -0,0 +1,13 @@
+package com.reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservicePrac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservicePrac1Application.class, args);
+	}
+
+}

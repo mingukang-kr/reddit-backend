6828dbd8e8fdb06528ed9c8ce19549d7ccaf6975
mangojuicedeli <mgkang0317@gmail.com>
2020-06-10 10:55:35 +0900

    Comment, Vote 레파지토리 추가

---
+++ src/main/java/com/reddit/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.reddit.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.reddit.model.Comment;
+import com.reddit.model.Post;
+import com.reddit.model.User;
+
+public interface CommentRepository extends JpaRepository<Post, Long> {
+
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);	
+}

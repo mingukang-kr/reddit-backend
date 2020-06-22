ba9248e5a08dbe4b966335d318a5c942b6676df7
mangojuicedeli <mgkang0317@gmail.com>
2020-06-22 16:57:03 +0900

    Swagger와 springfox 설정 완료

--- src/main/java/com/reddit/SpringbootWebservicePrac1Application.java
+++ src/main/java/com/reddit/SpringbootWebservicePrac1Application.java
@@ -2,9 +2,13 @@ package com.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import com.reddit.config.SwaggerConfiguration;
+
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 @EnableAsync
 public class SpringbootWebservicePrac1Application {
 
